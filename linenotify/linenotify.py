dd5b2de59d05cc0948218e68ecc527afc37876a5
Victor Gau <victorgau@gmail.com>
2019-11-13 23:56:20 +0800

    update linenotify.py

--- linenotify/linenotify.py
+++ linenotify/linenotify.py
@@ -1,16 +1,19 @@
 import requests
 
 # reference: https://notify-bot.line.me/doc/en/
-
-def Notify(token, msg):
-    headers = {
-        "Authorization": "Bearer " + token, 
-        "Content-Type" : "application/x-www-form-urlencoded"
-    }
-
-    payload = {'message': msg}
-    r = requests.post("https://notify-api.line.me/api/notify", headers = headers, params = payload)
+# reference: https://jkjung-avt.github.io/line-notify/
+def Notify(token, msg="Hello", stickerPackageId=None, stickerId=None, img=None):
+    headers = {'Authorization': 'Bearer ' + token}
+    if stickerPackageId and stickerId:
+        payload = {'message':msg, 'stickerPackageId':stickerPackageId, 'stickerId':stickerId}
+    else:
+        payload = {'message': msg}
+    files = {'imageFile': open(img, 'rb')} if img else None
+    r = requests.post("https://notify-api.line.me/api/notify", headers=headers, params=payload, files=files)
+    if files:
+        files['imageFile'].close()
     return r.status_code
+
 
 if __name__=="__main__":
     # 欲傳送的訊息內容
---
+++ linenotify/readme.md
@@ -0,0 +1,6 @@
+# Line Notify 說明
+
+Line Notify 網址：https://notify-bot.line.me/zh_TW/
+
+Line 貼圖清單: https://devdocs.line.me/files/sticker_list.pdf
+
